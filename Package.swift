af3eae16b87fe0ee2ca9db7ce4742f49a8480272
RickStreet <Rick_1@me.com>
2023-09-25 10:38:34 -0700

    Fixed StringKit dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIStringKit",
-            dependencies: []),
+            dependencies: ["StringKit"]),
         .testTarget(
             name: "UIStringKitTests",
             dependencies: ["UIStringKit", "StringKit"]),
