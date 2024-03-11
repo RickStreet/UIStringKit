9147c28cbace131986924611809f1ae882c0da4b
RickStreet <Rick_1@me.com>
2024-03-11 12:11:41 -0700

    Added utf8Encode() to StringKit.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/RickStreet/StringKit.git", from: "1.0.44")
+        .package(url: "https://github.com/RickStreet/StringKit.git", from: "1.0.52")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
